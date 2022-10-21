7a4b2eb8a21fb8df22507bbd46f8a12e7153c25f
Don Thorp <don@donthorp.net>
2022-10-21 15:14:26 -0500

    Fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 # Generate new github key pair for this machine
-ssh-keygen -t ed25519 -C "don@donthorp.net" -f ~/.ssh/`hostname`-github -N ""
-mkdir -p ~/.ssh/config.d
-chmod 700 ~/.ssh/config.d
 
-cat > ~/.ssh/config.d/github.com <<EOF
+#ssh-keygen -t ed25519 -C "don@donthorp.net" -f ~/.ssh/`hostname`-github -N ""
+#mkdir -p ~/.ssh/config.d
+#chmod 700 ~/.ssh/config.d
+
+cat > ~/.ssh/config.d/github.com << EOF
 Host github.com
   User git
   Port 22
@@ -14,7 +15,16 @@ Host github.com
   TCPKeepAlive yes
   IdentitiesOnly yes
   AddKeysToAgent yes
-EOF 
+EOF
+
+cat > ~/.gitconfig << GIT
+[user]
+	email = don@donthorp.net
+	name = Don Thorp
+[pull]
+	ff = only
+	rebase = true
+GIT
 
 echo Visit https://github.com/settings/keys and add this machines public key
 cat ~/.ssh/`hostname`-github.pub
