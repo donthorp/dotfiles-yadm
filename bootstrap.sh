743a8789eb3ec6e0db86b274a2f49aae4d4acd56
Don Thorp <don@pop-os.localdomain>
2022-10-21 14:49:53 -0500

    Here we go

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+# Generate new github key pair for this machine
+ssh-keygen -t ed25519 -C "don@donthorp.net" -f ~/.ssh/`hostname`-github -N ""
+mkdir -p ~/.ssh/config.d
+chmod 700 ~/.ssh/config.d
+
+cat > ~/.ssh/config.d/github.com <<EOF
+Host github.com
+  User git
+  Port 22
+  Hostname github.com
+  IdentityFile ~/.ssh/`hostname`-github.pub
+  TCPKeepAlive yes
+  IdentitiesOnly yes
+  AddKeysToAgent yes
+EOF 
+
+echo Visit https://github.com/settings/keys and add this machines public key
+cat ~/.ssh/`hostname`-github.pub
