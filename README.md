a22eaa319cd87153ea19bbb4da3fdbc33fa72e66
Don Thorp <don@donthorp.net>
2022-10-25 14:04:20 -0500

    Started readme and new bootstrap process

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dotfiles managed by `yadm`
+
+## Configure Host Name
+
+Use artemisNN for desktop machines.
+
+```bash
+sudo hostname -b artemis02
+```
+
+Use borosNN for laptops
+
+```bash
+sudo hostname -b boros02
+```
+
+Use hermesNN for virtual machines
+
+```bash
+hostnamectl set-hostname hermes01
+```
+
+## Used Names
+boros01 - Oryx Pro
+artemis01 - Gaming Machine - 23 Aug 2022
+hermes01 - Development VM for new dot files
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,48 @@
 #! /usr/bin/env bash
 
+# Please set the hostname of the machine BEFORE bootstrapping
+
+which yadm > /dev/null
+
+if [ $? -eq 1 ]; then
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt install -y yadm
+fi
+
 # Generate new github key pair for this machine
 
-#ssh-keygen -t ed25519 -C "don@donthorp.net" -f ~/.ssh/`hostname`-github -N ""
-#mkdir -p ~/.ssh/config.d
-#chmod 700 ~/.ssh/config.d
+KEYFILE=~/.ssh/`hostname`-github -N ""
+PUBKEYFILE=~/.ssh/`hostname`-github.pub
+CONFIGFILE=~/.ssh/config.d/github.com
 
-cat > ~/.ssh/config.d/github.com << EOF
-Host github.com
-  User git
-  Port 22
-  Hostname github.com
-  IdentityFile ~/.ssh/`hostname`-github.pub
-  TCPKeepAlive yes
-  IdentitiesOnly yes
-  AddKeysToAgent yes
-EOF
+if [ ! -f "$KEYFILE" ]; then
 
-cat > ~/.gitconfig << GIT
-[user]
-	email = don@donthorp.net
-	name = Don Thorp
-[pull]
-	ff = only
-	rebase = true
-GIT
+  ssh-keygen -t ed25519 -C "don@donthorp.net" -f ~/.ssh/`hostname`-github -N ""
 
-echo Visit https://github.com/settings/keys and add this machines public key
-cat ~/.ssh/`hostname`-github.pub
+  if [ ! -d "~/.ssh/config.d" ]; then
+    mkdir -p ~/.ssh/config.d
+    chmod 700 ~/.ssh/config.d
+  fi
+
+  if [ ! -f "$CONFIGFILE" ]; then
+
+    cat > $CONFIGFILE <<- EOF
+	Host github.com
+	  User git
+	  Port 22
+	  Hostname github.com
+	  IdentityFile $PUBKEYFILE 
+	  TCPKeepAlive yes
+	  IdentitiesOnly yes
+	  AddKeysToAgent yes
+	EOF
+  fi
+
+  echo Visit https://github.com/settings/keys and add this machines public key
+  cat $PUBKEYFILE 
+  
+fi
+
+
+
